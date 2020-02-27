1f9b4c71dd01ef3b693f3dab66abfdadd8026872
Yury Filimonov <yuriy.filimonov@gmail.com>
2020-02-27 21:36:02 +0300

    Initial commit

---
+++ desktop/src/rpg/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package rpg.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import rpg.game.RPGGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RPGGame(), config);
+	}
+}
